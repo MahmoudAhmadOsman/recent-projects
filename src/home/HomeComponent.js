b997475223603026601233afdf62837cb76ea698
Mahmoud Osman <mahmoudosman.som@gmail.com>
2024-01-30 00:47:09 -0600

    added Loading & NotFound components

--- src/home/HomeComponent.js
+++ src/home/HomeComponent.js
@@ -142,7 +142,7 @@ const HomeComponent = () => {
 									key={project.id}
 								>
 									<div className="card">
-										<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+										<span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-light text-dark">
 											{project.id}
 										</span>
 										<img
